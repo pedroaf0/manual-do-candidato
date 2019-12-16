f281b694d94046c7b1d30d0f30ce693ec5afab32
pedroaf0 <pedroaf1233@gmail.com>
2019-12-16 16:14:39 -0300

    Updates

---
+++ precache-manifest.c1b88b69e9f1a1100f2ed2a4f6f795a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7791b5becd2593fa8bfe01ae260fa91",
+    "url": "/manual-do-candidato/index.html"
+  },
+  {
+    "revision": "ba967ac3b214bdc54fc7",
+    "url": "/manual-do-candidato/static/css/main.ac73fee4.chunk.css"
+  },
+  {
+    "revision": "964e7ba0327c809663fd",
+    "url": "/manual-do-candidato/static/js/2.6e710cb3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/manual-do-candidato/static/js/2.6e710cb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba967ac3b214bdc54fc7",
+    "url": "/manual-do-candidato/static/js/main.14751412.chunk.js"
+  },
+  {
+    "revision": "e489cf29f8faef15907f",
+    "url": "/manual-do-candidato/static/js/runtime-main.acf6e639.js"
+  },
+  {
+    "revision": "d7c4d79496698d801040bd73e9dc8ac7",
+    "url": "/manual-do-candidato/static/media/logo.d7c4d794.svg"
+  }
+]);
